4a979c7378c9999f55fc0986a51a7bbcd56eabaf
Evalldas <jagminas.evaldas@gmail.com>
2019-11-18 16:14:24 +0200

    edited checkbox code

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -1,46 +1,56 @@
-<div class="row">
-    <div class="col-lg-7">
-        <form method="POST" id="clientTaskForm" action="<?=base_url()?>ClientTasker/">
-            <h4>Užduotys:</h4>
-            <input type="password" name="password" placeholder="Slaptažodis">
-            <button class="btn btn-danger" name="task" value="reboot">Restart</button><br><br>
-            <select name="serverIp" class="btn btn-dark">
-                <?php foreach($servers as $server) {?>
-                    <option value="<?=$server['ip'];?>"><?=$server['name'];?></option>
-                <?php }?>
-            </select>
-            <button class="btn btn-danger" name="task" value="installJcats">Install Jcats</button>
-            <button class="btn btn-danger" name="task" value="removeJcats">Remove Jcats</button>
-            <button class="btn btn-danger" name="task" value="changeServer">Change server</button>
-        </form>
-        <h3 class="col-title">Klientai</h3>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Pavadinimas</th>
-                    <th>IP adresas</th>
-                    <th>Serveris</th>
-                    <th><input type="checkbox" onClick="toggle(this)" /> Pasirinkti visus<br/></th>
-                </tr>
-            </thead>
-            <?php foreach($clients as $client) {?>
-                <tr>
-                    <td><?=$client["name"]?></td>
-                    <td><?=$client["ip"]?></td>
-                    <td><?=$servers[$client["server_id"]]["name"]?></td>
-                    <td><input form="clientTaskForm" type="checkbox" name="clientCheckbox[]" value="<?=$client['id']?>"><br/></td>
-                </tr>
+<div class="task-menu">
+    <form method="POST" id="clientTaskForm" action="<?=base_url()?>ClientTasker/">
+        <input type="password" name="password" placeholder="Slaptažodis">
+        <button class="btn btn-primary" name="task" value="reboot">Restart</button><br><br>
+        <select name="serverIp" class="btn btn-outline-secondary">
+            <?php foreach($servers as $server) {?>
+            <option value="<?=$server['ip'];?>"><?=$server['name'];?></option>
             <?php }?>
-        </table>
-    </div>
+        </select>
+        <button class="btn btn-primary" name="task" value="installJcats">Install Jcats</button>
+        <button class="btn btn-primary" name="task" value="removeJcats">Remove Jcats</button>
+        <button class="btn btn-primary" name="task" value="changeServer">Change server</button>
+    </form>
 </div>
+<table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th>Name</th>
+            <th>IP address</th>
+            <th>Server
+                <span class="server-info-icon">
+                    <span class="fas fa-info-circle"></span>
+                    <span class="server-info-message">This column shows
+                        what server the station is linked to</span>
+                </span>
+            </th>
+            <th>
+                <label for="checkbox" class="checkbox">
+                    <input type="checkbox" onClick="toggle(this)" style="margin-right: 5px;" />
+                    Check all
+                </label>
+            </th>
+        </tr>
+    </thead>
+    <?php foreach($clients as $client) {?>
+    <tr>
+        <td><?=$client["name"]?></td>
+        <td><?=$client["ip"]?></td>
+        <td><?=$servers[$client["server_id"]]["name"]?></td>
+        <td>
+            <label for="checkbox" class="checkbox">
+                <input form="clientTaskForm" type="checkbox" name="clientCheckbox[]" value="<?=$client['id']?>">
+            </label>
+        </td>
+    </tr>
+    <?php }?>
+</table>
 
 <script language="JavaScript">
-    function toggle(source) {
-        checkboxes = document.getElementsByName('clientCheckbox[]');
-        for(var i=0, n=checkboxes.length;i<n;i++) {
-            checkboxes[i].checked = source.checked;
-        }
+function toggle(source) {
+    checkboxes = document.getElementsByName('clientCheckbox[]');
+    for (var i = 0, n = checkboxes.length; i < n; i++) {
+        checkboxes[i].checked = source.checked;
     }
-
+}
 </script>
